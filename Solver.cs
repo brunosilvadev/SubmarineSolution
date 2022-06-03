e129b07dc28fe60808dcbb4b9d23feeba2aeafc0
Bruno Silva <brunosilva.dev@gmail.com>
2022-06-03 10:46:38 -0500

    Completed first puzzle

--- Solver.cs
+++ Solver.cs
@@ -1,25 +1,28 @@
 public class Solver{
     public string Solve() {
-        int horizontal = 0;
-        int vertical = 0;
-        const string path = "instructions.txt";
+        long horizontal = 0;
+        long vertical = 0;
+        const string path = @"../../../instructions.txt";
         string[] readText = File.ReadAllLines(path);
+
         foreach(var s in readText){
             string[] instruction = s.Split(' ');
             if(instruction.Length > 0){
                 switch (instruction[0]){
                     case "forward":
-                    horizontal = horizontal + Convert.ToInt32(instruction[1]);
+                    horizontal = horizontal + Convert.ToInt64(instruction[1]);
                     break;
                     case "down":
-                    vertical = vertical - Convert.ToInt32(instruction[1]);
+                    vertical = vertical + Convert.ToInt64(instruction[1]);
                     break;
                     case "up":
-                    vertical = vertical + Convert.ToInt32(instruction[1]);
+                    vertical = vertical - Convert.ToInt64(instruction[1]);
                     break;
                 }
             }
         }
-        return "The answer is ";
+
+
+        return "Based on the first interpretation, the puzzle answer is " + (horizontal * vertical).ToString();
     }
 }
