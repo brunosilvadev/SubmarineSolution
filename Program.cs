ed55b7426e6adb80c15ea96be87705512ab23fa5
Bruno Silva <brunosilva.dev@gmail.com>
2022-06-03 11:04:19 -0500

    Added second puzzle solution

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Solver s = new Solver();
-Console.WriteLine(s.Solve());
+﻿const string path = @"../../../instructions.txt";
+
+Solver s = new Solver(path);
+Console.WriteLine(s.SolveFirstPuzzle());
+Console.WriteLine(s.SolveSecondPuzzle());
--- Solver.cs
+++ Solver.cs
@@ -1,11 +1,23 @@
 public class Solver{
-    public string Solve() {
-        long horizontal = 0;
-        long vertical = 0;
-        const string path = @"../../../instructions.txt";
-        string[] readText = File.ReadAllLines(path);
 
-        foreach(var s in readText){
+    private string[] readFile { get; set; }
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="path"></param>
+    public Solver(string path)
+    {
+        this.readFile = File.ReadAllLines(path);
+    }    
+    /// <summary>
+    /// Solution to the first challenge in the puzzle
+    /// </summary>
+    /// <returns></returns>
+    public string SolveFirstPuzzle() {
+        long horizontal = 0;
+        long vertical = 0;        
+
+        foreach(var s in this.readFile){
             string[] instruction = s.Split(' ');
             if(instruction.Length > 0){
                 switch (instruction[0]){
@@ -24,5 +36,38 @@ public class Solver{
 
 
         return "Based on the first interpretation, the puzzle answer is " + (horizontal * vertical).ToString();
+    }
+    /// <summary>
+    /// Solution to the second challenge in the puzzle
+    /// </summary>
+    /// <returns></returns>
+    public string SolveSecondPuzzle()
+    {
+        long horizontal = 0;
+        long aim = 0;
+        long depth = 0;
+
+        foreach (var s in this.readFile)
+        {
+            string[] instruction = s.Split(' ');
+            if (instruction.Length > 0)
+            {
+                switch (instruction[0])
+                {
+                    case "forward":
+                        horizontal = horizontal + Convert.ToInt64(instruction[1]);
+                        depth = depth + (aim * Convert.ToInt64(instruction[1]));
+                        break;
+                    case "down":
+                        aim = aim + Convert.ToInt64(instruction[1]);
+                        break;
+                    case "up":
+                        aim = aim - Convert.ToInt64(instruction[1]);
+                        break;
+                }
+            }
+        }
+
+        return "Based on the second interpretation, the puzzle answer is " + (horizontal * depth).ToString();
     }
 }
